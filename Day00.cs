325b79438f12c094f3f81684fc8abe996f29c9e9
Jake Dunkley <jakeddunkley@gmail.com>
2025-11-30 10:00:36 -0500

    Part 2 of test day

--- Day00.cs
+++ Day00.cs
@@ -27,6 +27,20 @@ public class Day00 : IDay
             diffs.Add(Math.Abs(leftLocationIds[i] - rightLocationIds[i]));
         }
 
-        DisplayResult(diffs.Sum(), 1);
+        DisplayResult(diffs.Sum(), part: 1);
+
+        /*
+            Part02
+        */
+
+        Dictionary<int, int> similarityScores = leftLocationIds
+            .GroupBy(lid => lid)
+            .ToDictionary(g => g.Key, g => rightLocationIds.Where(rlid => rlid == g.Key).Count());
+        
+        int sum = similarityScores
+            .Select(ss => ss.Key * ss.Value)
+            .Sum();
+        
+        DisplayResult(sum, part: 2);
     }
 }
